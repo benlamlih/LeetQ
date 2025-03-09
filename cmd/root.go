4d739b85359157fa33707f2f11972e2af6e9bf9a
Mohammed Benlamlih <45820673+benlamlih@users.noreply.github.com>
2025-03-09 17:22:56 +0000

    adjust the cli description

--- cmd/root.go
+++ cmd/root.go
@@ -8,16 +8,16 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "zero",
-	Short: "zero is a cli tool for performing basic mathematical operations",
-	Long:  "zero is a cli tool for performing basic mathematical operations - addition, multiplication, division and subtraction.",
+	Use:   "lt",
+	Short: "LeetQ is a CLI tool for fetching random LeetCode problems and test cases.",
+	Long:  "LeetQ is a CLI tool that retrieves random LeetCode problems, including descriptions and example test cases, for efficient problem-solving.",
 	Run: func(cmd *cobra.Command, args []string) {
 	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Oops. An error while executing Zero '%s'\n", err)
+		fmt.Fprintf(os.Stderr, "Oops. An error while executing LeetQ '%s'\n", err)
 		os.Exit(1)
 	}
 }
